322d18cbcdcc10ffd7a52042c402fa18c24ef6fc
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-06-19 14:11:05 +0300

    commit

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Bu Alan Zoruludur")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Bu Alan Zoruludur")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Bu Alan Zoruludur")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Bu Alan Zoruludur")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Bu Alan Zoruludur")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Bu Alan Zoruludur")]
+        [Compare("Password", ErrorMessage = "Şifreler Aynı Olmalı")]
+        public string ConfirmPassword { get; set; }
+    }
+}
