336cb6e21c3f95e1377a3bad1ff914463d361c8a
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-06-20 17:00:16 +0300

    Commit

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void TDelete(About item)
+        {
+            _aboutDal.Delete(item);
+        }
+
+        public About TGetById(int id)
+        {
+            return _aboutDal.GetById(id);
+        }
+
+        public List<About> TGetList()
+        {
+            return _aboutDal.GetList();
+        }
+
+        public void TInsert(About item)
+        {
+            _aboutDal.Insert(item);
+        }
+
+        public void TUpdate(About item)
+        {
+            _aboutDal.Update(item);
+        }
+    }
+}
