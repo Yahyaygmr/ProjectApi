3a640c6e1e29de028bcb6cbb25fa365b879cf757
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-06-20 23:36:00 +0300

    Commit

--- Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
+++ Frontend/HotelProject.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Dtos.SubscribeDto;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public PartialViewResult _SubscribePartial()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<PartialViewResult> _SubscribePartial(CreateSubscribeDto createSubscribeDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return PartialView();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSubscribeDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5027/api/Subscribe", content);
+
+                return PartialView();
+
         }
     }
 }
---
+++ Frontend/HotelProject.WebUI/Dtos/StaffDto/ResultStaffDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelProject.WebUI.Dtos.StaffDto
+{
+    public class ResultStaffDto
+    {
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public string StaffTitle { get; set; }
+        public string SocialMedia1 { get; set; }
+        public string SocialMedia2 { get; set; }
+        public string SocialMedia3 { get; set; }
+    }
+}
